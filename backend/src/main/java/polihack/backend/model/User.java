afa0fd04e3172966ceeaea7515f9703e0c0c2baa
Pal Roland <palroland2004@gmail.com>
2025-12-05 20:48:43 +0200

    backend structure 1

--- backend/src/main/java/polihack/backend/model/User.java
+++ backend/src/main/java/polihack/backend/model/User.java
@@ -1,4 +1,19 @@
 package polihack.backend.model;
 
+import jakarta.persistence.*;
+import lombok.Data;
+
+import static org.postgresql.jdbc.EscapedFunctions.USER;
+
+@Entity
+@Table(name= USER)
+@Data
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "username")
+    private String username;
 }
