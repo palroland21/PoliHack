aa1b637968ab7ad039a786613e5c20fe0ec9bd98
Vasile Breban <vbreban09@gmail.com>
2025-12-05 23:19:21 +0200

    frontend- added home view

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -4,26 +4,26 @@ import VictimFlow from '../views/VictimFlow.vue'
 import RescuerHub from '../views/RescuerHub.vue'
 
 const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: HomeView
-  },
-  {
-    path: '/victim',
-    name: 'VictimFlow',
-    component: VictimFlow
-  },
-  {
-    path: '/rescuer',
-    name: 'RescuerHub',
-    component: RescuerHub
-  }
+    {
+        path: '/',
+        name: 'home',
+        component: HomeView
+    },
+    {
+        path: '/victim',
+        name: 'victim',
+        component: VictimFlow
+    },
+    {
+        path: '/rescuer',
+        name: 'rescuer',
+        component: RescuerHub
+    }
 ]
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
-  routes
+    history: createWebHistory(import.meta.env.BASE_URL),
+    routes
 })
 
 export default router
